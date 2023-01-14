aab1f9ca55675aa0221524f46d1b11035ac26bc2
icju <isicju@gmail.com>
2023-01-14 15:19:20 +0000

    2

---
+++ src/main/java/com/vkatit/controller/CitizenController.java
@@ -0,0 +1,23 @@
+package com.vkatit.controller;
+
+
+import com.vkatit.model.Citizen;
+import com.vkatit.service.CitizenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CitizenController {
+
+    @Autowired
+    CitizenService citizenService;
+
+    @GetMapping("/citizens")
+    public List<Citizen> getFirst10Citizens() {
+        return citizenService.getFirst10Citizens();
+    }
+
+}
--- src/main/java/com/vkatit/service/CitizenService.java
+++ src/main/java/com/vkatit/service/CitizenService.java
@@ -1,4 +1,20 @@
 package com.vkatit.service;
 
+import com.vkatit.model.Citizen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CitizenService {
+
+    @Autowired
+    List<Citizen> citizens;
+
+    public List<Citizen> getFirst10Citizens() {
+        return citizens.subList(0, 10);
+    }
+
 }
