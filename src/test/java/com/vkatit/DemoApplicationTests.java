f6f92f55c1de638a413ac81762e9ef7ca0adc64c
icju <isicju@gmail.com>
2023-01-15 01:17:02 +0000

    fixtest

--- src/test/java/com/vkatit/DemoApplicationTests.java
+++ src/test/java/com/vkatit/DemoApplicationTests.java
@@ -1,40 +1,45 @@
 package com.vkatit;
 
-import static org.hamcrest.CoreMatchers.*;
-
 import com.vkatit.model.Country;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+    @LocalServerPort
+    int randomPort;
+    RestTemplate restTemplate;
 
-	RestTemplate restTemplate;
+    @BeforeEach
+    public void init() {
+        restTemplate = new RestTemplate();
+    }
 
-	@BeforeEach
-	public void init(){
-		restTemplate = new RestTemplate();
-	}
+    @Autowired
+    Environment environment;
 
-	@Autowired
-	AppContext appContext;
+    @Autowired
+    ApplicationContext applicationContext;
 
-	@Test
-	void contextLoads() {
-		MatcherAssert.assertThat(appContext, is(notNullValue()));
-	}
+    @Test
+    void contextLoads() {
+        MatcherAssert.assertThat(applicationContext, is(notNullValue()));
+    }
 
-	@Test
-	public void testCountries(){
-		Country[] countries = restTemplate.getForObject("http://localhost:8080/countries", Country[].class);
-		MatcherAssert.assertThat(countries, is(notNullValue()));
-		MatcherAssert.assertThat(countries.length, is(equalTo(6)));
-	}
+    @Test
+    public void testCountries() throws InterruptedException {
+        Country[] countries = restTemplate.getForObject("http://localhost:" + randomPort + "/countries", Country[].class);
+        MatcherAssert.assertThat(countries, is(notNullValue()));
+        MatcherAssert.assertThat(countries.length, is(equalTo(6)));
+    }
 
 }
