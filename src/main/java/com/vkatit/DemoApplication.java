21aede567162acfe4a3b117d942f2a36d725df38
icju <isicju@gmail.com>
2023-02-04 15:06:54 +0000

    kotik

--- src/main/java/com/vkatit/DemoApplication.java
+++ src/main/java/com/vkatit/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+        System.out.println();
     }
 
 }
