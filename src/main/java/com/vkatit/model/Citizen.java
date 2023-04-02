381d01d9174a620d0027d4f85a4f88bec921da43
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-02 18:00:35 +0400

    commit

--- src/main/java/com/vkatit/model/Citizen.java
+++ src/main/java/com/vkatit/model/Citizen.java
@@ -1,9 +1,12 @@
 package com.vkatit.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -13,7 +16,8 @@ public class Citizen {
     @JsonProperty("firstname")
     private String firstName;
     @JsonProperty("birthDate")
-    private String birthDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime birthDate;
     @JsonProperty("address")
     private String address;
     @JsonProperty("country")
--- src/main/java/com/vkatit/service/CitizenService.java
+++ src/main/java/com/vkatit/service/CitizenService.java
@@ -1,8 +1,25 @@
 package com.vkatit.service;
 
+import com.vkatit.model.Citizen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CitizenService {
+    @Autowired
+    public CitizenService(List<Citizen> citizenList) {
+        this.citizenList = citizenList;
+    }
+    @Qualifier("citizenList")
+    List<Citizen> citizenList;
+    public List<Citizen> citizensLivingInThatCountry(String country) {
+        return citizenList.stream()
+                .filter(citizen -> citizen.getCountry() == "Russia")
+                .collect(Collectors.toList());
+    }
 
 }
