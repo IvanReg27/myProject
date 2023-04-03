273cfbe2f2f881117853b5a6f399515c3c9d2f70
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-03 21:25:26 +0400

    commit

--- src/main/java/com/vkatit/model/Citizen.java
+++ src/main/java/com/vkatit/model/Citizen.java
@@ -3,12 +3,14 @@ package com.vkatit.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Citizen {
