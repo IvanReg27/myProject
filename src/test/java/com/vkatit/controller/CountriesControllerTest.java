1733bc7efad1a513175719ea928327b948a32a75
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-08 00:32:13 +0400

    commit

---
+++ src/test/java/com/vkatit/controller/CountriesControllerTest.java
@@ -0,0 +1,12 @@
+package com.vkatit.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountriesControllerTest {
+
+    @Test
+    void getAllCounties() {
+    }
+}
