1ad4dce9808cd114c0fac8ea9d52772b8da5a41b
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-16 18:26:13 +0400

    commit

--- src/main/java/com/vkatit/employee/Employee.java
+++ src/main/java/com/vkatit/employee/Employee.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Employee {
 
     private String firstName;
--- src/test/java/com/vkatit/JacksonConvertTest.java
+++ src/test/java/com/vkatit/JacksonConvertTest.java
@@ -3,7 +3,10 @@ package com.vkatit;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vkatit.employee.Employee;
+import com.vkatit.employee.JsonField;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
 
 public class JacksonConvertTest {
     ObjectMapper objectMapper = new ObjectMapper();
