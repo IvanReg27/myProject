81c314cbab83675fe54078812d45e035f0d7f878
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-17 16:14:50 +0400

    commit

--- src/main/java/com/vkatit/applicationContext/ApplicationContext.java
+++ src/main/java/com/vkatit/applicationContext/ApplicationContext.java
@@ -1,4 +1,23 @@
 package com.vkatit.applicationContext;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ApplicationContext {
+    private final Map<Class<?>, Object> applicationContext = new HashMap<>();
+    public ApplicationContext() {
+
+        List<String> list = new ArrayList<>();
+        list.add("String in list ONE");
+        list.add("String in list TWO");
+
+        applicationContext.put(List.class, list);
+        applicationContext.put(Integer.class, 17);
+        applicationContext.put(String.class, "MyNameIvan");
+    }
+    public <T> T getInjectToClassAppContext(Class<T> clazz) {
+        return clazz.cast(applicationContext.get(clazz));
+    }
 }
---
+++ src/main/java/com/vkatit/applicationContext/InjectToClassAppContext.java
@@ -0,0 +1,28 @@
+package com.vkatit.applicationContext;
+
+import java.util.List;
+
+public class InjectToClassAppContext {
+    @Inject
+    private List<String> list;
+    @Inject
+    private Integer integer;
+    @Inject
+    private String string;
+
+    public InjectToClassAppContext() {
+        integer = 1988;
+        list = List.of("1988", "2018", "2022");
+        string = "default value";
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+    public Integer getInteger() {
+        return integer;
+    }
+    public String getString() {
+        return string;
+    }
+}
