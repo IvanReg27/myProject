b612ca3a0da63c1c852da2ec54eaa4c979901786
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-19 18:04:15 +0400

    commit

--- src/main/java/com/vkatit/lesson15_2/ThreadRunnable.java
+++ src/main/java/com/vkatit/lesson15_2/ThreadRunnable.java
@@ -2,26 +2,31 @@ package com.vkatit.lesson15_2;
 
 import java.util.ArrayList;
 
-public class Main {
+public class ThreadRunnable {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Main thread...");
 
         Thread ivanThread = new Thread(new Runnable() {
             @Override
             public void run() {
+
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
                 System.out.println("Start thread: " + Thread.currentThread().getName() + " process...");
 
                 int a = (int) (Math.random() * 1_000_000);
-
                 ArrayList<Integer> number = new ArrayList<Integer>();
                 number.add(a);
 
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
                 System.out.println("Finished thread: " + "random number -> " + number);
             }
         }, "ivan thread");
