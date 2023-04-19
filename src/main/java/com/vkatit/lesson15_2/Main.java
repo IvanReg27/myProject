708df3e0300da0b368537e5d38cd3bc4b4e21cda
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-19 13:01:24 +0400

    commit

--- src/main/java/com/vkatit/lesson15_2/Main.java
+++ src/main/java/com/vkatit/lesson15_2/Main.java
@@ -5,17 +5,26 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Main thread...");
 
-        Thread thread = new Thread(new Runnable() {
+        Thread ivanThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("Start thread " + Thread.currentThread().getName() + "process");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Start thread: " + Thread.currentThread().getName() + " process...");
 
                 int a = (int) (Math.random() * 1_000_000);
 
                 ArrayList<Integer> number = new ArrayList<Integer>();
                 number.add(a);
+
+                System.out.println("Finished thread: " + "random number -> " + number);
             }
         }, "ivan thread");
+        ivanThread.start();
     }
 }
