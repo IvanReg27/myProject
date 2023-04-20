a43e50d7f846e69a1af4c2b61e33f45a71281e46
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-20 14:46:56 +0400

    commit

--- src/main/java/com/vkatit/lesson15_3/ThreadPollRecFile.java
+++ src/main/java/com/vkatit/lesson15_3/ThreadPollRecFile.java
@@ -1,5 +1,6 @@
 package com.vkatit.lesson15_3;
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,7 +12,8 @@ public class ThreadPollRecFile {
 
     ExecutorService executor = Executors.newFixedThreadPool(10);
     SimpleDateFormat sdf = null;
-    private  final  int  COUNT = 5;
+    private final int COUNT = 5;
+
     ThreadPollRecFile() {
 
         sdf = new SimpleDateFormat("HH:mm:ss.S");
@@ -35,24 +37,28 @@ public class ThreadPollRecFile {
             cdl2.await();
             cdl3.await();
             cdl4.await();
-        } catch(InterruptedException exc) {
+        } catch (InterruptedException exc) {
         }
         executor.shutdown();
 
         System.out.println("Finished threads");
     }
+
     void printMessage(final String templ) {
         String text = sdf.format(new Date()) + " : " + templ;
         System.out.println(text);
     }
+
     class MyThread implements Runnable {
         String name;
         CountDownLatch latch;
+
         MyThread(CountDownLatch c, String n) {
             latch = c;
             name = n;
             new Thread(this);
         }
+
         public void run() {
             try {
                 //---------------------------------------------------------
@@ -60,21 +66,39 @@ public class ThreadPollRecFile {
                 ArrayList<Integer> number = new ArrayList<Integer>();
                 number.add(a);
                 //---------------------------------------------------------
+                try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                    String inputLine;
+                    final StringBuilder content = new StringBuilder();
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
 
+                        try (final PrintWriter writer = new PrintWriter(new File("C:\\Users\\Ivan\\IdeaProjects\\lesson3_maven\\src\\main\\resources\\CSVDemo.csv"))) {
+                            final StringBuilder sb = new StringBuilder();
+                            sb.append(content);
 
-
-
-
-                for(int i = 0; i < COUNT; i++) {
-                    printMessage(name + " - " + number);
-                    latch.countDown();
-                    Thread.sleep((int)(Math.random() * 1500));
+                            writer.write(sb.toString());
+                            writer.close();
+                            System.out.println("Done!");
+                        } catch (FileNotFoundException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                    //--------------------------------------------------------
+                    for (int i = 0; i < COUNT; i++) {
+                        printMessage(name + " - " + number);
+                        latch.countDown();
+                        Thread.sleep((int) (Math.random() * 1500));
+                    }
+                    printMessage(name + " completed");
+                } catch (InterruptedException e) {
                 }
-                printMessage(name + " completed");
-            } catch (InterruptedException e) {}
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-    }
-    public static void main(String args[]) {
-        new ThreadPollRecFile();
+
+        public static void main(String args[]) {
+            new ThreadPollRecFile();
+        }
     }
 }
