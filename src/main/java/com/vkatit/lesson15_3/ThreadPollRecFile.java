c7533243876ca42d70a457b2e7c430961b4bc9de
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-20 15:25:36 +0400

    commit

--- src/main/java/com/vkatit/lesson15_3/ThreadPollRecFile.java
+++ src/main/java/com/vkatit/lesson15_3/ThreadPollRecFile.java
@@ -9,11 +9,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ThreadPollRecFile {
-
-    ExecutorService executor = Executors.newFixedThreadPool(10);
     SimpleDateFormat sdf = null;
-    private final int COUNT = 5;
-
+    private  final  int  COUNT = 5;
     ThreadPollRecFile() {
 
         sdf = new SimpleDateFormat("HH:mm:ss.S");
@@ -37,68 +34,48 @@ public class ThreadPollRecFile {
             cdl2.await();
             cdl3.await();
             cdl4.await();
-        } catch (InterruptedException exc) {
+        } catch(InterruptedException exc) {
         }
         executor.shutdown();
 
         System.out.println("Finished threads");
     }
-
     void printMessage(final String templ) {
         String text = sdf.format(new Date()) + " : " + templ;
         System.out.println(text);
     }
-
     class MyThread implements Runnable {
         String name;
         CountDownLatch latch;
-
         MyThread(CountDownLatch c, String n) {
             latch = c;
             name = n;
             new Thread(this);
         }
-
         public void run() {
             try {
-                //---------------------------------------------------------
+                //-------------------------------------------------
                 int a = (int) (Math.random() * 1_000_000);
                 ArrayList<Integer> number = new ArrayList<Integer>();
                 number.add(a);
-                //---------------------------------------------------------
-                try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-                    String inputLine;
-                    final StringBuilder content = new StringBuilder();
-                    while ((inputLine = in.readLine()) != null) {
-                        content.append(inputLine);
-
-                        try (final PrintWriter writer = new PrintWriter(new File("C:\\Users\\Ivan\\IdeaProjects\\lesson3_maven\\src\\main\\resources\\CSVDemo.csv"))) {
-                            final StringBuilder sb = new StringBuilder();
-                            sb.append(content);
-
-                            writer.write(sb.toString());
-                            writer.close();
-                            System.out.println("Done!");
-                        } catch (FileNotFoundException e) {
-                            System.out.println(e.getMessage());
-                        }
-                    }
-                    //--------------------------------------------------------
-                    for (int i = 0; i < COUNT; i++) {
-                        printMessage(name + " - " + number);
-                        latch.countDown();
-                        Thread.sleep((int) (Math.random() * 1500));
-                    }
-                    printMessage(name + " completed");
-                } catch (InterruptedException e) {
+                //-------------------------------------------------
+            try(FileWriter writer = new FileWriter(new File("C:\\Users\\Ivan\\IdeaProjects\\lesson3_maven\\src\\main\\resources\\CSVDemo.csv"))) {
+                    writer.write(String.valueOf(number));
+                    writer.flush();
                 }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+                catch(IOException ex){
+                }
+                //-------------------------------------------------
+                for(int i = 0; i < COUNT; i++) {
+                    printMessage(name + " - " + number);
+                    latch.countDown();
+                    Thread.sleep((int)(Math.random() * 1500));
+                }
+                printMessage(name + " completed");
+            } catch (InterruptedException e) {}
         }
-
-        public static void main(String args[]) {
-            new ThreadPollRecFile();
-        }
+    }
+    public static void main(String args[]) {
+        new ThreadPollRecFile();
     }
 }
