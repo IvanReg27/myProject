309645f9a7460d76d4f919f8be6ac8c104c34968
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-24 00:20:05 +0400

    commit

--- src/main/java/com/vkatit/service/CitizenService.java
+++ src/main/java/com/vkatit/service/CitizenService.java
@@ -2,16 +2,13 @@ package com.vkatit.service;
 
 import com.vkatit.model.Citizen;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 @Service
 public class CitizenService {
     @Autowired
     List<Citizen> citizens;
-
     public void addNewCitizen(Citizen citizen) {
         citizens.add(citizen);
     }
--- src/test/java/com/vkatit/DemoApplicationTests.java
+++ src/test/java/com/vkatit/DemoApplicationTests.java
@@ -12,34 +12,27 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.CoreMatchers.*;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
     @LocalServerPort
     int randomPort;
     RestTemplate restTemplate;
-
     @BeforeEach
     public void init() {
         restTemplate = new RestTemplate();
     }
-
     @Autowired
     Environment environment;
-
     @Autowired
     ApplicationContext applicationContext;
-
     @Test
     void contextLoads() {
         MatcherAssert.assertThat(applicationContext, is(notNullValue()));
     }
-
     @Test
     public void testCountries() {
         Country[] countries = restTemplate.getForObject("http://localhost:" + randomPort + "/countries", Country[].class);
         MatcherAssert.assertThat(countries, is(notNullValue()));
         MatcherAssert.assertThat(countries.length, is(equalTo(6)));
     }
-
 }
