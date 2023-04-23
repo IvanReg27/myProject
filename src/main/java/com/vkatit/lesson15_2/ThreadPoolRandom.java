843761f788c24e7bc6168d5b0266dc4c22365ccc
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-23 23:19:22 +0400

    commit

--- src/main/java/com/vkatit/lesson15_2/ThreadPoolRandom.java
+++ src/main/java/com/vkatit/lesson15_2/ThreadPoolRandom.java
@@ -1,7 +1,6 @@
 package com.vkatit.lesson15_2;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
