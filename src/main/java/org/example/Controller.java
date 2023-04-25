4ba5a8174e727055f4641c05cca520c34132a633
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-25 14:47:54 +0400

    commit

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -13,28 +13,18 @@ public class Controller {
    static Map<DELIVERY_CHANNEL, NotificationService> lookup = new HashMap<>();
 
     public static void init(String[] args) {
-        String emailName = "53ffc361c9d9692e9060ef89c18f1c11"; //check  in group
-        String emailPassword = "47db15682b550bdf44e2609e94c0dd4a"; //check  in group
+        String emailName = "xxx";
+        String emailPassword = "xxx";
         NotificationService emailService =  new EmailNotificationService(emailName, emailPassword);
 
-        String name = "isicju@gmail.com"; //check  in group
-        String password = "MjHqfCWBKh63W12Ognw5aM4XzR"; //check in group
+        String name = "xxx@gmail.com";
+        String password = "xxx";
 
         NotificationService smsService = new SmsNotificationService(name, password);
-
-        //Пока не получается смс текст направить в Json формате (согласно ссылки...которую ты дал)
-        /*ObjectMapper objectMapper = new ObjectMapper();
-        SmsNotificationService smsConvertToJson = new SmsNotificationService(name, password);
-        objectMapper.writeValue(new File("target/smsConvertToJson.json"), smsConvertToJson);*/
 
         lookup.put(DELIVERY_CHANNEL.SMS, emailService);
         lookup.put(DELIVERY_CHANNEL.EMAIL, smsService);
     }
-
-//    public void sendMessage(DELIVERY_CHANNEL deliveryChannel, String recipient, String content) {
-//        lookup.get(DELIVERY_CHANNEL.SMS).sendMessage(recipient, content);
-//    }
-
     enum DELIVERY_CHANNEL {
         SMS, EMAIL, TELEGRAM
     }
